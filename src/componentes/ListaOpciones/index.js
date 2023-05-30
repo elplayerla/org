ce60824c31b10f8afa998afc5d5c65e59fceabf1
Brayan Sánchez <elplayer_la@hotmail.com>
2023-05-29 23:04:04 -0600

    Se actualiza código

---
+++ src/componentes/ListaOpciones/index.js
@@ -0,0 +1,23 @@
+import "./ListaOpciones.css"
+
+const ListaOpciones = (props) => {
+
+    //Método map -> arreglo.map( (equipo, index) => {
+    //      return <option></option>
+    // });
+
+    const manejarCambio = (event) => {
+        props.actualizarEquipo(event.target.value)
+    }
+
+
+    return <div className="lista-opciones">
+        <label>Equipos</label>
+        <select value={props.valor} onChange={manejarCambio} required={props.required}>
+            <option value="" disabled defaultValue="" hidden>Seleccionar equipo</option>
+            {props.equipos.map((equipo, index) => <option key={index} value={equipo}>{equipo}</option>)}
+        </select>
+    </div>
+}
+
+export default ListaOpciones
